00cf90d965e31a3c51617c9e638677ac893d73b8
yeonil <ahnyi527@gmail.com>
2022-09-21 11:24:22 +0900

    회원정보 수정/ 회원가입 수정본

--- static/common.js
+++ static/common.js
@@ -38,6 +38,24 @@ function sign_up() {
         $("#help-password2").text("비밀번호가 일치합니다.").removeClass("is-danger").addClass("is-success")
     }
 
+    if (nickname == "") {
+        alert("닉네임을 입력해주세요.")
+        $("#input-nickname").focus()
+        return
+    }
+
+    if (cat_name == "") {
+        alert("고양이 이름을 입력해주세요.")
+        $("#input-cat_name").focus()
+        return
+    }
+
+    if (file == "") {
+        alert("고양이 사진을 업로드해주세요.")
+        $("#file").focus()
+        return
+    }
+
     let form_data = new FormData()
 
     form_data.append("file", file)
@@ -59,6 +77,86 @@ function sign_up() {
             window.location.replace("/")
         }
     });
+
+}
+
+function update_user() {
+    let user_id = $("#hidden-user_id").val()
+    let password = $("#input-password").val()
+    let password2 = $("#input-password2").val()
+    let nickname = $("#input-nickname").val()
+    let cat_name = $("#input-cat_name").val()
+    let intro = $("#input-intro").val()
+    let file = $('#file')[0].files[0]
+    let fileName = $('#fileName').val()
+
+    let form_data = new FormData()
+
+    if (password == "" && password2 == ""){
+        if(!confirm("비밀번호 변경 없이 회원 정보를 수정하시겠습니까?"))
+            return
+    } else {
+        if (password == "") {
+            $("#help-password").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+            $("#input-password").focus()
+            return;
+        } else if (!is_password(password)) {
+            $("#help-password").text("비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자").removeClass("is-safe").addClass("is-danger")
+            $("#input-password").focus()
+            return
+        } else {
+            $("#help-password").text("사용할 수 있는 비밀번호입니다.").removeClass("is-danger").addClass("is-success")
+        }
+        if (password2 == "") {
+            $("#help-password2").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+            $("#input-password2").focus()
+            return;
+        } else if (password2 != password) {
+            $("#help-password2").text("비밀번호가 일치하지 않습니다.").removeClass("is-safe").addClass("is-danger")
+            $("#input-password2").focus()
+            return;
+        } else {
+            $("#help-password2").text("비밀번호가 일치합니다.").removeClass("is-danger").addClass("is-success")
+        }
+
+        form_data.append("password", password)
+    }
+
+    if (nickname == "") {
+        alert("닉네임을 입력해주세요.")
+        $("#input-nickname").focus()
+        return
+    }
+
+    if (cat_name == "") {
+        alert("고양이 이름을 입력해주세요.")
+        $("#input-cat_name").focus()
+        return
+    }
+
+    form_data.append("user_id", user_id)
+    form_data.append("fileName", fileName)
+    form_data.append("file", file)
+    form_data.append("nickname", nickname)
+    form_data.append("cat_name", cat_name)
+    form_data.append("intro", intro)
+
+    $.ajax({
+        type: "POST",
+        url: "/update_user",
+        data: form_data,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            alert("회원정보를 수정하였습니다!")
+            window.location.replace("/")
+        }
+    });
+
+}
+
+function default_password(){
 
 }
 
